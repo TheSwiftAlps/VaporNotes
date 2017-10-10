ba39b870ed936735438b8fce97046e27dcd736fa
Adrian Kosmaczewski <learn@akosma.training>
2017-10-10 19:32:57 +0200

    Removed template comments

--- Tests/AppTests/NoteControllerTests.swift
+++ Tests/AppTests/NoteControllerTests.swift
@@ -5,20 +5,10 @@ import Sockets
 @testable import Vapor
 @testable import App
 
-/// This file shows an example of testing an
-/// individual controller without initializing
-/// a Droplet.
-
 class NoteControllerTests: TestCase {
     let initialMessage = "I'm a note"
     let updatedMessage = "I have been updated \(Date())"
 
-    /// For these tests, we won't be spinning up a live
-    /// server, and instead we'll be passing our constructed
-    /// requests programmatically
-    /// this is usually an effective way to test your
-    /// application in a convenient and safe manner
-    /// See RouteTests for an example of a live server test
     let controller = NoteController()
 
     func testNoteRoutes() throws {

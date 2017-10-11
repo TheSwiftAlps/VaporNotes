8a5f05b3849ba9f6df8bd41dd71f56ab3ab2f436
Adrian Kosmaczewski <learn@akosma.training>
2017-10-11 15:11:46 +0200

    Made a more intelligent command, that also tests

--- Sources/App/Setup/BuildAndRunCommand.swift
+++ Sources/App/Setup/BuildAndRunCommand.swift
@@ -3,7 +3,7 @@ import Console
 
 public final class BuildAndRunCommand: Command {
     public let id = "build-run"
-    public let help = ["This commands performs a build and run operation in one shot"]
+    public let help = ["This commands performs build, test and run operations in one shot"]
     public let console: ConsoleProtocol
 
     public init(console: ConsoleProtocol) {
@@ -13,6 +13,8 @@ public final class BuildAndRunCommand: Command {
     public func run(arguments: [String]) throws {
         console.print("==> vapor build")
         try console.foregroundExecute(program: "vapor", arguments: ["build"])
+        console.print("==> vapor test")
+        try console.foregroundExecute(program: "vapor", arguments: ["test"])
         console.print("==> vapor run serve")
         try console.foregroundExecute(program: "vapor", arguments: ["run", "serve"])
     }

0dca3973192a2541ba13c00e592689b4e16358cd
Adrian Kosmaczewski <learn@akosma.training>
2017-10-21 17:23:54 +0200

    Fixed the version header for the tests

--- Sources/App/Middleware/VersionMiddleware.swift
+++ Sources/App/Middleware/VersionMiddleware.swift
@@ -4,7 +4,7 @@ final class VersionMiddleware: Middleware {
     func respond(to request: Request, chainingTo next: Responder) throws -> Response {
         let response = try next.respond(to: request)
 
-        response.headers["Version"] = "Swift Alps Notes API v1.0"
+        response.headers["Version"] = "Swift Alps 2017 – Notes API 1.0"
         return response
     }
 }
